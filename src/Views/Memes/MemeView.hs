9933e0496c14cc2d78e4d755996e1dc286e97f9c
Mikail Khan <mikail.khan45@gmail.com>
2021-01-03 01:25:31 -0500

    cleanup, added incomplete resume

--- src/Views/Memes/MemeView.hs
+++ src/Views/Memes/MemeView.hs
@@ -5,6 +5,7 @@ module Views.Memes.MemeView where
 import qualified Data.Text                      as T
 import           Text.Blaze.Html5               ((!))
 import qualified Text.Blaze.Html5               as H
+import           Text.Blaze.Html5.Attributes    (style, href, class_, src)
 import qualified Text.Blaze.Html5.Attributes    as A
 
 import           Control.Monad                  (forM_)
@@ -13,9 +14,9 @@ import           Views.Memes.MemeData
 
 navButtons :: Int -> H.Html
 navButtons pageNum =
-    H.div ! A.style "display: flex; justify-content: space-between; width: 98vw" $ do
-        H.a ! A.href prevPage ! A.class_ "btn" $ tToHTML "Previous Page"
-        H.a ! A.href nextPage ! A.class_ "btn" $ tToHTML "Next Page"
+    H.div ! style "display: flex; justify-content: space-between; width: 98vw" $ do
+        H.a ! href prevPage ! class_ "btn" $ tToHTML "Previous Page"
+        H.a ! href nextPage ! class_ "btn" $ tToHTML "Next Page"
     where nextPage = H.toValue $ "/memes/" <> (show $ pageNum + 1)
           prevPage = H.toValue $ "/memes/" <> (show $ pageNum - 1)
 
@@ -24,16 +25,16 @@ memeContentHTML :: Meme -> H.Html
 memeContentHTML Meme { ty=ty, title=title, url=url } =
     case ty of
       Image -> 
-          H.a ! A.id titleVal ! A.href urlVal $ do
-              H.img ! A.class_ "meme" ! A.src urlVal ! A.alt "meme"
+          H.a ! A.id titleVal ! href urlVal $ do
+              H.img ! class_ "meme" ! src urlVal ! A.alt "meme"
 
       MP4 -> 
-          H.video ! A.id titleVal ! A.class_ "meme" ! A.controls "bar" $ do
-              H.source ! A.src urlVal ! A.type_ "video/mp4"
+          H.video ! A.id titleVal ! class_ "meme" ! A.controls "bar" $ do
+              H.source ! src urlVal ! A.type_ "video/mp4"
               tToHTML "Unsupported browser for videos"
 
       Youtube ->
-          H.iframe ! A.id titleVal ! A.title "Youtube Video" ! A.class_ "meme" ! A.src urlVal $ do
+          H.iframe ! A.id titleVal ! A.title "Youtube Video" ! class_ "meme" ! src urlVal $ do
               tToHTML "Youtube Video"
     where 
         urlVal = H.toValue url
@@ -41,8 +42,8 @@ memeContentHTML Meme { ty=ty, title=title, url=url } =
 
 memeGrid :: Meme -> H.Html
 memeGrid meme@(Meme { ty=ty, title=title, url=url }) =
-    H.div ! A.class_ "grid-entry" $ do
-        H.h2 $ H.a ! A.id titleVal ! A.href ("#" <> titleVal) $ H.toHtml title
+    H.div ! class_ "grid-entry" $ do
+        H.h2 $ H.a ! A.id titleVal ! href ("#" <> titleVal) $ H.toHtml title
         memeContentHTML meme
     where titleVal = H.toValue title
 
@@ -51,8 +52,8 @@ memeHTML pageNum memes =
     H.html $ do
         H.head $ do
             H.title $ tToHTML "bad memes"
-            H.link ! A.rel "stylesheet" ! A.href "/CSS/Memes.css"
+            H.link ! A.rel "stylesheet" ! href "/CSS/Memes.css"
         H.body $ do
             navButtons pageNum
-            H.div ! A.class_ "grid" $ forM_ memes memeGrid
+            H.div ! class_ "grid" $ forM_ memes memeGrid
             navButtons pageNum
