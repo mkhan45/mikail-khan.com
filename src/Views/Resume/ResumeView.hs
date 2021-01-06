16265d8574dbd1f31d6b83f1a1e6d857c360a0af
Mikail Khan <mikail.khan45@gmail.com>
2021-01-06 16:45:02 -0500

    fancy circle lines ul

--- src/Views/Resume/ResumeView.hs
+++ src/Views/Resume/ResumeView.hs
@@ -3,36 +3,39 @@
 module Views.Resume.ResumeView where
 
 import qualified Data.Text                      as T
+import           Data.List                      (partition)
 import           Text.Blaze.Html5               (script, meta, dataAttribute, h1, h2, img, html, a, li, ul, input, body, nav, link, (!))
 import qualified Text.Blaze.Html5               as H
 import           Text.Blaze.Html5.Attributes    (class_, rel, href, for, type_, src, content, name)
 import qualified Text.Blaze.Html5.Attributes    as A
 
+import           Control.Monad                  (forM_)
+
 import           Views.Resume.ResumeData
 
--- I feel like this should exist
--- elements matching go on the lft
--- others go on the right
-sieve :: (a -> Bool) -> [a] -> ([a], [a])
-sieve f ls = go f ls ([], [])
-    where go :: (a -> Bool) -> [a] -> ([a], [a]) -> ([a], [a])
-          go _ []     (lhs, rhs) = (lhs, rhs)
-          go f (x:xs) (lhs, rhs) = if f x then go f xs (x:lhs, rhs)
-                                            else go f xs (lhs, x:rhs)
+repeatStars :: Int -> H.Html
+repeatStars n = H.span $ forM_ [1..n] (\_ -> starHTML)
+    where starID = H.toValue $ T.pack "ei-star"
+          starHTML :: H.Html
+          starHTML = H.span ! class_ "star-icon" ! dataAttribute "icon" starID ! dataAttribute "size" "s" $ "★"
 
 skillsHTML :: [Skill] -> H.Html
 skillsHTML skills = do
     H.div ! A.class_ "resumeSection skills" $ do
         H.h2 "Skills"
         H.hr
-        H.span $ H.toHtml ("5+ Years: " <> skillString five_year)
-        H.br
-        H.span $ H.toHtml ("3+ Years: " <> skillString three_year)
-        H.br
-        H.span $ H.toHtml ("1+ Years: " <> skillString one_year)
+        H.ul $ do
+            H.li $ H.h3 $ repeatStars 5
+            H.li $ H.toHtml $ skillString five_year
+        H.ul $ do
+            H.li $ H.h3 $ repeatStars 3
+            H.li $ H.toHtml $ skillString three_year
+        H.ul $ do
+            H.li $ H.h3 $ repeatStars 1
+            H.li $ H.toHtml $ skillString one_year
     where 
-        (five_year, less_five) = sieve ((>=5) . skillExperience) skills
-        (three_year, less_three) = sieve ((>=3) . skillExperience) less_five
+        (five_year, less_five) = partition ((>=5) . skillExperience) skills
+        (three_year, less_three) = partition ((>=3) . skillExperience) less_five
         one_year = filter ((>=1) . skillExperience) less_three
 
         skillString :: [Skill] -> T.Text
@@ -52,8 +55,8 @@ experiencesHTML experiences = do
 experienceEntry :: Experience -> H.Html
 experienceEntry experience = do
     H.div ! A.class_ "experienceEntry" $ do
-        H.h3 name
         H.ul $ do
+            H.li $ H.h3 name
             mapM_ descEntry (experienceDescription experience)
     where name = H.toHtml $ experienceName experience
 
@@ -67,8 +70,8 @@ educationsHTML educations = do
 educationEntry :: Education -> H.Html
 educationEntry education = do
     H.div ! A.class_ "educationEntry" $ do
-        H.h3 name
         H.ul $ do
+            H.li $ H.h3 name
             mapM_ descEntry (educationDescription education)
     where name = H.toHtml $ educationName education
 
@@ -78,6 +81,8 @@ resumeHTML (Resume skills experiences educations) =
         H.head $ do
             H.title "Mikail Khan"
             link ! rel "stylesheet" ! href "/CSS/base.css"
+            link ! rel "stylesheet" ! href "https://cdn.jsdelivr.net/npm/evil-icons@1.9.0/assets/evil-icons.min.css"
+            script ! src "https://cdn.jsdelivr.net/npm/evil-icons@1.9.0/assets/evil-icons.min.js" $ mempty
             meta ! name "viewport" ! content "width=device-width, initial-scale=1.0"
         body ! A.class_ "resumeContainer flex-center" $ do
             H.br
